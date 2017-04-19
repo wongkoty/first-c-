941298b03c5e13a86830b3acb3d053fdc8887209
koty wong <wongkoty@gmail.com>
2017-04-19 14:19:20 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String string1 = "This is a string";
+            Console.WriteLine(string1);
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+            PrintFooToScreen100Times();
+
+            if(keyInfo.KeyChar == 'a')
+            {
+                Console.WriteLine("That is not a number");
+            }
+            else
+            {
+                Console.WriteLine("Did you type {0}", keyInfo.KeyChar.ToString());
+            }
+        }
+
+        static void PrintFooToScreen()
+        {
+            Console.WriteLine("foo");
+        }
+        static void PrintFooToScreen100Times()
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                PrintFooToScreen();
+;           }
+        }
+    }
+}
